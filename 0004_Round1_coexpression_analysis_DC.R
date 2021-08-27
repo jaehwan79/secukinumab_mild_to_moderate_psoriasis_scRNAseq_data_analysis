b925d8d8a8d5e2290bd4a974a5885f1f88967992
jaehwan79 <66520492+jaehwan79@users.noreply.github.com>
2021-08-26 22:05:11 -0700

    Update 0004_Round1_coexpression_analysis_DC.R

--- 0004_Round1_coexpression_analysis_DC.R
+++ 0004_Round1_coexpression_analysis_DC.R
@@ -1,59 +1,3 @@
-#load library ####
-#instRound0.packages('Seurat')
-library(Seurat)
-library(RNAransform)
-library(dplyr)
-library(ggplot2)
-#source("http://bioconductor.org/biocLite.R")
-#biocLite("GEOquery")
-library(GEOquery)
-#source("https://bioconductor.org/biocLite.R")
-#biocLite("DropletUtils")
-library(DropletUtils)
-#instRound0.packages("devtools")
-library(devtools)
-#sourceters
-#Start ####("https://raw.githubusercontent.com/farrellja/URD/master/URD-InstRound0.R")
-library(URD)
-#source("https://bioconductor.org/biocLite.R")
-#biocLite("edgeR")
-library(edgeR)
-library(reticulate)
-library(scales)
-library(forcats)
-library(cowplot)
-#install.packages("magrittr")
-library(magrittr)
-#install.packages('varhandle')
-library(varhandle)
-#install.packages("viridis")
-library(viridis)
-#install.packages('data.table')
-library(data.table) 
-library(googlesheets4)
-######################################################################################################3
-# Round1 combining clusters ####
-setwd("~/Dropbox/NSK_0937/NSK_0937_Experiment_Data/NSK_0937_scRNAseq/NSK_0937_scRNAseq_Data_processed/Psoriasis_posttx_05.01.2021")
-setwd("D:/Dropbox/NSK_0937/NSK_0937_Experiment_Data/NSK_0937_scRNAseq/NSK_0937_scRNAseq_Data_processed/Psoriasis_posttx_05.01.2021")
-
-dir.create("./coexpression_DCs")
-setwd("./coexpression_DCs")
-getwd()
-setwd("~/Dropbox/NSK_0937/NSK_0937_Experiment_Data/NSK_0937_scRNAseq/NSK_0937_scRNAseq_Data_processed/Psoriasis_posttx_05.01.2021/coexpression_DCs")
-setwd("D:/Dropbox/NSK_0937/NSK_0937_Experiment_Data/NSK_0937_scRNAseq/NSK_0937_scRNAseq_Data_processed/Psoriasis_posttx_05.01.2021/coexpression_DCs")
-
-list.files()
-rm(list= ls()[!(ls() %in% c('Round1'))])
-
-
-gene_subset  <- read_sheet("https://docs.google.com/spreadsheets/d/19nhrzGudrn8ihl4RASuyGlBe7JMHdWnnWKoZ0i2rZ4s/edit#gid=0")
-gene_subset <- as.matrix(unique(gene_subset[1]))
-
-#load("/Users/jkim05/Dropbox/NSK_0937/NSK_0937_Experiment_Data/NSK_0937_scRNAseq/NSK_0937_scRNAseq_Data_processed/Psoriasis_posttx_05.01.2021/combined_data/Round0_integrated_analyzed_01.31.2021.Rda")
-#load("/Users/jkim05/Dropbox/NSK_0937/NSK_0937_Experiment_Data/NSK_0937_scRNAseq/NSK_0937_scRNAseq_Data_processed/Psoriasis_posttx_05.01.2021/combined_data/Round0_integrated_analyzed_01.31.2021.Rda")
-#load("D:/Dropbox/NSK_0937/NSK_0937_Experiment_Data/NSK_0937_scRNAseq/NSK_0937_scRNAseq_Data_processed/Psoriasis_posttx_05.01.2021/combined_data/Round0_integrated_analyzed_02.09.2020.Rda")
-#load("D:/Dropbox/NSK_0937/NSK_0937_Experiment_Data/NSK_0937_scRNAseq/NSK_0937_scRNAseq_Data_processed/Psoriasis_posttx_05.01.2021/Round1/combined_data/Round1_integrated_analyzed_02.15.2020.Rda")
-
 ######################################################################################################3
 # ClusterNames_0.8_Serial ####
 Idents(Round1)
@@ -124,7 +68,9 @@ list[,c(6,7,8)] <- list[,c(2,3,4)]
 colnames(list)[c(6,7,8)] <- paste(colnames(list[,c(2,3,4)]), "_Proportion", sep = "")
 list[,c(6,7,8)] <- list[,c(6,7,8)]/list[,5]*100
 
-
+#Control sample number 10
+#PosstTx sample number 4
+#Pretx sample number 11
 list[grep("Control", list$Cluster),c(2:4) ] <- list[grep("Control", list$Cluster),c(2:4) ]/10
 list[grep("PostTx", list$Cluster),c(2:4) ] <- list[grep("PostTx", list$Cluster),c(2:4) ]/4
 list[grep("PreTx", list$Cluster),c(2:4) ] <- list[grep("PreTx", list$Cluster),c(2:4) ]/11
@@ -236,7 +182,9 @@ list[,c(6,7,8)] <- list[,c(2,3,4)]
 colnames(list)[c(6,7,8)] <- paste(colnames(list[,c(2,3,4)]), "_Proportion", sep = "")
 list[,c(6,7,8)] <- list[,c(6,7,8)]/list[,5]*100
 
-
+#Control sample number 10
+#PosstTx sample number 4
+#Pretx sample number 11
 list[grep("Control", list$Cluster),c(2:4) ] <- list[grep("Control", list$Cluster),c(2:4) ]/10
 list[grep("PostTx", list$Cluster),c(2:4) ] <- list[grep("PostTx", list$Cluster),c(2:4) ]/4
 list[grep("PreTx", list$Cluster),c(2:4) ] <- list[grep("PreTx", list$Cluster),c(2:4) ]/11
@@ -344,7 +292,9 @@ list[,c(6,7,8)] <- list[,c(2,3,4)]
 colnames(list)[c(6,7,8)] <- paste(colnames(list[,c(2,3,4)]), "_Proportion", sep = "")
 list[,c(6,7,8)] <- list[,c(6,7,8)]/list[,5]*100
 
-
+#Control sample number 10
+#PosstTx sample number 4
+#Pretx sample number 11
 list[grep("Control", list$Cluster),c(2:4) ] <- list[grep("Control", list$Cluster),c(2:4) ]/10
 list[grep("PostTx", list$Cluster),c(2:4) ] <- list[grep("PostTx", list$Cluster),c(2:4) ]/4
 list[grep("PreTx", list$Cluster),c(2:4) ] <- list[grep("PreTx", list$Cluster),c(2:4) ]/11
